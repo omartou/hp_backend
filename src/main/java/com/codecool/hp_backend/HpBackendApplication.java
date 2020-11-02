470438623809fc5553c44e04a6a438596278f152
KriszProg <krisz.codecool@gmail.com>
2020-11-02 10:40:09 +0100

    initial commit

---
+++ src/main/java/com/codecool/hp_backend/HpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.hp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HpBackendApplication.class, args);
+    }
+
+}
