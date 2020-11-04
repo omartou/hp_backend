561b26d0f3e50227737e3516bf98586445d944cf
MMaGH <misk.mardi@gmail.com>
2020-11-04 14:21:30 +0100

    add hogwarts employee filter to dataHandler

--- src/main/java/com/codecool/hp_backend/controller/HogwartsController.java
+++ src/main/java/com/codecool/hp_backend/controller/HogwartsController.java
@@ -6,23 +6,18 @@ import com.codecool.hp_backend.service.JsonHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.json.*;
 import java.io.FileNotFoundException;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/hogwarts")
 public class HogwartsController {
 
-    private final JsonHandler jsonHandler;
-
     private final DataHandler dataHandler;
 
     @Autowired
-    HogwartsController(JsonHandler jsonHandler, DataHandler dataHandler) {
-        this.jsonHandler = jsonHandler;
+    HogwartsController(DataHandler dataHandler) {
         this.dataHandler = dataHandler;
     }
 
@@ -32,17 +27,7 @@ public class HogwartsController {
     }
 
     @GetMapping("/employees")
-    public String getEmployees() throws FileNotFoundException {
-        JsonArray characters = jsonHandler.readCharactersFromFile();
-
-        List<JsonObject> employeeCharacters = characters.getValuesAs(JsonObject.class)
-                .stream()
-                .filter(obj -> obj.containsKey("role"))
-                .filter(obj -> obj.get("role").toString().split(",")[0].equals("Professor"))
-                .collect(Collectors.toList());
-
-        return employeeCharacters.toString();
+    public List<PotterCharacter> getEmployees() {
+        return dataHandler.getHogwartsEmployees();
     }
-
-
 }
--- src/main/java/com/codecool/hp_backend/service/DataHandler.java
+++ src/main/java/com/codecool/hp_backend/service/DataHandler.java
@@ -11,14 +11,15 @@ import java.util.List;
 public class DataHandler {
 
     private CharacterStorage characterStorage;
+    private final List<PotterCharacter> characters;
 
     @Autowired
     public  DataHandler(CharacterStorage characterStorage) {
         this.characterStorage = characterStorage;
+        this.characters = characterStorage.getCharacterList();
     }
 
     public List<PotterCharacter> getHogwartsHouseCharacters(String house) {
-        List<PotterCharacter> characters = characterStorage.getCharacterList();
         List<PotterCharacter> houseCharacters = new ArrayList<>();
 
         for (PotterCharacter character : characters) {
@@ -27,5 +28,15 @@ public class DataHandler {
             }
         }
         return houseCharacters;
+    }
+
+    public List<PotterCharacter> getHogwartsEmployees() {
+        List<PotterCharacter> employees = new ArrayList<>();
+        for (PotterCharacter character : characters) {
+            if(character.getRole() != null && character.getRole().split(",")[0].equals("Professor")) {
+                employees.add(character);
+            }
+        }
+        return employees;
     }
 }
