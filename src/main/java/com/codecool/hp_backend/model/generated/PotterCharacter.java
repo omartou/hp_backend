9aca803f413cd738ad9c150a28cb96f1e0f3f8ac
KriszProg <krisz.codecool@gmail.com>
2020-11-05 06:45:12 +0100

    add @Getter annotation and delete explicit Getters

--- src/main/java/com/codecool/hp_backend/model/generated/PotterCharacter.java
+++ src/main/java/com/codecool/hp_backend/model/generated/PotterCharacter.java
@@ -1,7 +1,9 @@
 package com.codecool.hp_backend.model.generated;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
 
+@Getter
 public class PotterCharacter {
 
 	@JsonProperty("role")
@@ -55,71 +57,4 @@ public class PotterCharacter {
 	@JsonProperty("animagus")
 	private String animagus;
 
-	public String getRole(){
-		return role;
-	}
-
-	public String getBloodStatus(){
-		return bloodStatus;
-	}
-
-	public String getSchool(){
-		return school;
-	}
-
-	public String getSpecies(){
-		return species;
-	}
-
-	public boolean isDeathEater(){
-		return deathEater;
-	}
-
-	public int getV(){
-		return V;
-	}
-
-	public boolean isDumbledoresArmy(){
-		return dumbledoresArmy;
-	}
-
-	public String getName(){
-		return name;
-	}
-
-	public boolean isMinistryOfMagic(){
-		return ministryOfMagic;
-	}
-
-	public String getId(){
-		return id;
-	}
-
-	public boolean isOrderOfThePhoenix(){
-		return orderOfThePhoenix;
-	}
-
-	public String getHouse(){
-		return house;
-	}
-
-	public String getBoggart() {
-		return boggart;
-	}
-
-	public String getAlias() {
-		return alias;
-	}
-
-	public String getWand() {
-		return wand;
-	}
-
-	public String getPatronus() {
-		return patronus;
-	}
-
-	public String getAnimagus() {
-		return animagus;
-	}
 }
