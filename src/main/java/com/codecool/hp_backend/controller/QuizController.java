6c17dd55e13eed66402e5cc40cd5010f21f131d4
KriszProg <krisz.codecool@gmail.com>
2020-11-05 13:39:55 +0100

    add '@Crossorigin' annotation

--- src/main/java/com/codecool/hp_backend/controller/QuizController.java
+++ src/main/java/com/codecool/hp_backend/controller/QuizController.java
@@ -3,11 +3,13 @@ package com.codecool.hp_backend.controller;
 import com.codecool.hp_backend.service.DataHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/quiz")
 public class QuizController {
 
