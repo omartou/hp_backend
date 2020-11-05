46ee4acc117ffb3b3a22acb4e4a3faed55d51973
KriszProg <krisz.codecool@gmail.com>
2020-11-05 07:41:03 +0100

    add QuizController class

--- src/main/java/com/codecool/hp_backend/controller/QuizController.java
+++ src/main/java/com/codecool/hp_backend/controller/QuizController.java
@@ -1,2 +1,26 @@
-package com.codecool.hp_backend.controller;public class QuizController {
+package com.codecool.hp_backend.controller;
+
+import com.codecool.hp_backend.model.generated.PotterCharacter;
+import com.codecool.hp_backend.service.DataHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/quiz")
+public class QuizController {
+
+    private final DataHandler dataHandler;
+
+    @Autowired
+    public QuizController(DataHandler dataHandler) {
+        this.dataHandler = dataHandler;
+    }
+
+    @GetMapping("/house/random")
+    public PotterCharacter getRandomHouseQuizCharacter() {
+        return dataHandler.getRandomHouseQuizCharacter();
+    }
+
 }
