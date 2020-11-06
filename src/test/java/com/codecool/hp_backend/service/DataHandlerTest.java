9de9509f778f58dd6b0b580597c367fbb781f573
omartou <omar.toure@yahoo.com>
2020-11-06 20:32:14 +0100

    add getCharacterById() test function

--- src/test/java/com/codecool/hp_backend/service/DataHandlerTest.java
+++ src/test/java/com/codecool/hp_backend/service/DataHandlerTest.java
@@ -4,7 +4,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import com.codecool.hp_backend.model.generated.PotterCharacter;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -16,9 +18,41 @@ class DataHandlerTest {
     private static CharacterStorage characterStorage;
     private static DataHandler dataHandler;
     private final List<PotterCharacter> characters = new ArrayList<>();
-    
+
+    private static PotterApiService potterApiService;
+    private static Map<String, PotterCharacter> charactersTest = new LinkedHashMap<>();
+
+    private final List<PotterCharacter> houseQuizCharacters = new ArrayList<>();
+
+
     @BeforeEach
     void init() {
+        setupTestWithList();
+        setupTestWithHashMap();
+//        setupTestForGetRandomHouseQuizCharacter();
+    }
+
+//    private void setupTestForGetRandomHouseQuizCharacter() {
+//        houseQuizCharacters.add(new PotterCharacter("human", "Harry Potter", "0", "Gryffindor",
+//                "Hogwarts School of Witchcraft and Wizardry"));
+//        when(dataHandler.getRandomHouseQuizCharacter()).thenReturn(
+//                (PotterCharacter) houseQuizCharacters);
+//        dataHandler = new DataHandler(characterStorage);
+//    }
+
+    private void setupTestWithHashMap() {
+        charactersTest.put("0", new PotterCharacter("human", "Harry Potter", "0", "Gryffindor",
+                "Hogwarts School of Witchcraft and Wizardry"));
+        charactersTest.put("1", new PotterCharacter("human", "Draco Malfoy", "1", "Slytherin",
+                "Hogwarts School of Witchcraft and Wizardry"));
+        charactersTest.put("2", new PotterCharacter("ghost", "Bloody Baron", "2", "Slytherin",
+                "Hogwarts School of Witchcraft and Wizardry"));
+        potterApiService = mock(PotterApiService.class);
+        when(potterApiService.getAllCharacters()).thenReturn(charactersTest);
+        characterStorage = new CharacterStorage(potterApiService);
+    }
+
+    private void setupTestWithList() {
         characters.add(new PotterCharacter("human", "Harry Potter", "0", "Gryffindor",
                 "Hogwarts School of Witchcraft and Wizardry"));
         characters.add(new PotterCharacter("human", "Draco Malfoy", "1", "Slytherin",
@@ -41,7 +75,8 @@ class DataHandlerTest {
         List<PotterCharacter> houseCharacterList = new ArrayList<>();
         houseCharacterList.add(characters.get(1));
         houseCharacterList.add(characters.get(2));
-        assertIterableEquals(houseCharacterList, dataHandler.getHogwartsHouseCharacters("Slytherin"));
+        assertIterableEquals(houseCharacterList,
+                dataHandler.getHogwartsHouseCharacters("Slytherin"));
     }
 
     @Test
@@ -67,9 +102,14 @@ class DataHandlerTest {
 
     @Test
     void getCharacterById() {
+        PotterCharacter testCharacter = charactersTest.get("1");
+        assertEquals(testCharacter, characterStorage.getCharacterById("1"));
     }
 
     @Test
     void getRandomHouseQuizCharacter() {
+//        List<PotterCharacter> houseQuizCharacters = new ArrayList<>();
+//        houseQuizCharacters.add(characters.get(0));
+//        assertEquals(houseQuizCharacters.get(0), dataHandler.getRandomHouseQuizCharacter());
     }
 }
