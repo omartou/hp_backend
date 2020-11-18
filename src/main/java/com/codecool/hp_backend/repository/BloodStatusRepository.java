61dbdabe21d604a4ddeba2323712d91128babd83
KriszProg <krisz.codecool@gmail.com>
2020-11-18 11:00:15 +0100

    add query to all repository which returns 'name' by 'id'

--- src/main/java/com/codecool/hp_backend/repository/BloodStatusRepository.java
+++ src/main/java/com/codecool/hp_backend/repository/BloodStatusRepository.java
@@ -2,7 +2,12 @@ package com.codecool.hp_backend.repository;
 
 import com.codecool.hp_backend.entity.BloodStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface BloodStatusRepository extends JpaRepository<BloodStatus, Long> {
     BloodStatus findBloodStatusByName(String bloodStatus);
+
+    @Query("SELECT bs.name FROM BloodStatus bs WHERE bs.id = :id")
+    String getBloodStatusNameById(@Param("id") Long id);
 }
--- src/main/java/com/codecool/hp_backend/repository/SchoolRepository.java
+++ src/main/java/com/codecool/hp_backend/repository/SchoolRepository.java
@@ -2,7 +2,12 @@ package com.codecool.hp_backend.repository;
 
 import com.codecool.hp_backend.entity.School;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface SchoolRepository extends JpaRepository<School, Long> {
     School findSchoolByName(String school);
+
+    @Query("SELECT s.name FROM School s WHERE s.id = :id")
+    String getSchoolNameById(@Param("id") Long id);
 }
