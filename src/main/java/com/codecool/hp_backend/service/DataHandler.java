d6b467c1d44b6fccdd5215795c9abc80338c094b
KriszProg <krisz.codecool@gmail.com>
2020-11-18 09:51:23 +0100

    add 'DataHandler' interface

--- src/main/java/com/codecool/hp_backend/service/DataHandler.java
+++ src/main/java/com/codecool/hp_backend/service/DataHandler.java
@@ -1,83 +1,20 @@
 package com.codecool.hp_backend.service;
 
 import com.codecool.hp_backend.model.generated.PotterCharacter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.List;
 
-@Service
-public class DataHandler {
+public interface DataHandler {
 
-    private final CharacterStorage characterStorage;
-    private final List<PotterCharacter> characters;
-    private final List<PotterCharacter> houseQuizCharacters = new ArrayList<>();
+    List<PotterCharacter> getHogwartsHouseCharacters(String house);
 
+    List<PotterCharacter> getHogwartsEmployees();
 
-    @Autowired
-    public  DataHandler(CharacterStorage characterStorage) {
-        this.characterStorage = characterStorage;
-        this.characters = characterStorage.getCharacterList();
-        initHouseQuizCharacters();
-    }
+    List<PotterCharacter> getOtherCharacters();
 
-    private void initHouseQuizCharacters() {
-        for (PotterCharacter character : characters) {
-            if (character.getHouse() != null) {
-                houseQuizCharacters.add(character);
-            }
-        }
-    }
+    List<PotterCharacter> getMinistryOfMagicCharacters();
 
-    public List<PotterCharacter> getHogwartsHouseCharacters(String house) {
-        List<PotterCharacter> houseCharacters = new ArrayList<>();
+    PotterCharacter getCharacterById(String id);
 
-        for (PotterCharacter character : characters) {
-            if (character.getHouse() != null && character.getHouse().toLowerCase().equals(house.toLowerCase())) {
-                houseCharacters.add(character);
-            }
-        }
-        return houseCharacters;
-    }
-
-    public List<PotterCharacter> getHogwartsEmployees() {
-        List<PotterCharacter> employees = new ArrayList<>();
-        for (PotterCharacter character : characters) {
-            if(character.getRole() != null && character.getRole().split(",")[0].equals("Professor")) {
-                employees.add(character);
-            }
-        }
-        return employees;
-    }
-
-    public List<PotterCharacter> getOtherCharacters() {
-        List<PotterCharacter> otherCharacters = new ArrayList<>();
-        for (PotterCharacter character : characters) {
-            if(!character.isMinistryOfMagic()
-                    && !Objects.equals(character.getSchool(), "Hogwarts School of Witchcraft and Wizardry")){
-                otherCharacters.add(character);
-            }
-        }
-        return otherCharacters;
-    }
-
-    public List<PotterCharacter> getMinistryOfMagicCharacters() {
-        List<PotterCharacter> ministryCharacters = new ArrayList<>();
-        for (PotterCharacter character : characters) {
-            if (character.isMinistryOfMagic()) {
-                ministryCharacters.add(character);
-            }
-        }
-        return ministryCharacters;
-    }
-
-    public PotterCharacter getCharacterById(String id) {
-        return characterStorage.getCharacterById(id);
-    }
-
-    public PotterCharacter getRandomHouseQuizCharacter() {
-        Random random = new Random();
-        return houseQuizCharacters.get(random.nextInt(houseQuizCharacters.size()));
-    }
-
+    PotterCharacter getRandomHouseQuizCharacter();
 }
