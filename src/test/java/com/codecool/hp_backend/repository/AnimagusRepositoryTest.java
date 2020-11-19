7e3eeb3ae065a902dfd6a90f7d0c75a32574498a
MMaGH <misk.mardi@gmail.com>
2020-11-19 11:34:55 +0100

    add animagus repository test

---
+++ src/test/java/com/codecool/hp_backend/repository/AnimagusRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.codecool.hp_backend.repository;
+
+import com.codecool.hp_backend.entity.Animagus;
+import org.assertj.core.util.Lists;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@ActiveProfiles("test")
+public class AnimagusRepositoryTest {
+
+    @Autowired
+    private AnimagusRepository animagusRepository;
+
+    @Test
+    public void findAnimagusByName() {
+        Animagus animagusOne = Animagus.builder()
+                .name("animagusOne")
+                .build();
+
+        Animagus animagusTwo = Animagus.builder()
+                .name("animagusTwo")
+                .build();
+
+        animagusRepository.saveAll(Lists.newArrayList(animagusOne, animagusTwo));
+
+        Animagus returnedAnimagus = animagusRepository.findAnimagusByName("animagusTwo");
+        assertEquals(animagusTwo, returnedAnimagus);
+    }
+
+    @Test
+    public void getAnimagusNameById() {
+    }
+}
